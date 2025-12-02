0acbefad0c3f0c8608804d2ba39c736ac6ac4d8b
Vinay-Sriram-Tummidi <vinaysriram.tummidi@gmail.com>
2025-12-02 12:54:00 +0530

    Basic Structure Creation

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Navbar } from "./welcome/NavBar";
+import { GlobalProvider } from "./GlobalContext/GlobalContext";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,9 +35,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+        <GlobalProvider>
+     <Navbar/>
         {children}
         <ScrollRestoration />
         <Scripts />
+        </GlobalProvider>
+ 
       </body>
     </html>
   );
---
+++ app/welcome/NavBar.tsx
@@ -0,0 +1,92 @@
+import { Link } from "react-router";
+import { Menu } from "lucide-react";
+import { Button } from "../components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+} from "../components/ui/sheet";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { useContext } from "react";
+import { GlobalContext } from "~/GlobalContext/GlobalContext";
+
+export function Navbar() {
+
+  const ctx=useContext(GlobalContext);
+  if(!ctx) return null;
+
+  const {state,dispatch}=ctx;
+
+  console.log("State",state);
+  console.log("Dispatch",dispatch);
+  return (
+    <header className="w-full bg-white border-b fixed">
+      <div className="max-w-6xl mx-auto flex items-center justify-between p-4">
+        {/* Logo */}
+        <Link to="/" className="font-bold text-xl">
+          SmartCloud
+        </Link>
+
+        {/* Desktop Menu */}
+        <nav className="hidden md:flex items-center gap-6">
+          <Link to="/gold" className="text-sm hover:text-black">
+            Gold and Silver
+          </Link>
+          <Link to="/secured" className="text-sm hover:text-black">
+            Secured Credit Card
+          </Link>
+          <Link to="/bonds" className="text-sm hover:text-black">
+            Bonds
+          </Link>
+          {}
+             {state.user ? (
+          <>
+           <div className="flex items-center gap-2">
+  <span>Hello, <strong>{state.user}</strong></span>
+  <Avatar className="rounded-lg">
+    <AvatarImage
+      src="https://github.com/evilrabbit.png"
+      alt="@evilrabbit"
+    />
+    <AvatarFallback>VS</AvatarFallback>
+  </Avatar>
+</div>
+            <Button
+              onClick={() => dispatch({ type: "LOGOUT" })}
+              className="text-white px-3 py-1 rounded"
+            >
+              Logout
+            </Button>
+          </>
+        ) : (
+          <Button
+            onClick={() => dispatch({ type: "SET_USER", payload: "Vinay" })}
+            className=" text-white px-3 py-1 rounded"
+          >
+            Login
+          </Button>
+
+        )}
+        </nav>
+
+        {/* Mobile Menu */}
+        <Sheet>
+          <SheetTrigger className="md:hidden">
+            <Menu className="h-6 w-6" />
+          </SheetTrigger>
+
+          <SheetContent side="right" className="p-6">
+            <nav className="flex flex-col gap-4 text-lg">
+              <Link to="/gold">Gold and Silver</Link>
+              <Link to="/secured">Secured Credit Card</Link>
+              <Link to="/bonds">Bonds</Link>
+              <Button className="w-full mt-4" asChild>
+                <Link to="/login">Login</Link>
+              </Button>
+            </nav>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </header>
+  );
+}
